1d2093b0d4115833e015ce253791f035b53d5af1
Simon Monecke <Simon.Monecke@otto.de>
2015-10-14 09:34:21 +0200

    update to version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambdacd-lineup "0.1.0"
+(defproject lambdacd-lineup "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "The MIT License (MIT)"
